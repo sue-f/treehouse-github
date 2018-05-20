3aa79ebaa4a1795bf0e652b5ca158fecb7f42ce4
Sue F <suefatwork@gmail.com>
2018-05-20 18:28:08 -0400

    initial commit

---
+++ src/goodfirstprogram/GoodFirstProgram.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goodfirstprogram;
+
+/**
+ *
+ * @author Sue
+ */
+public class GoodFirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("Hello Again");
+        System.out.println("I'd much rather you 'not'.");
+        System.out.println("I \"said\" do not touch this.");
+        
+        System.out.println("Hens " + (25 + 30 / 6));
+        System.out.println(3 + 2 < 5 - 7);
+    }
+    
+}
